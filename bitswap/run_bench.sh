1c7e84748063ae892a7e40d871b29476e43c7ee0
rht <rhtbot@gmail.com>
2017-01-31 15:10:50 +0400

    More bitswap stat

--- bitswap/run_bench.sh
+++ bitswap/run_bench.sh
@@ -5,21 +5,21 @@ PID_DAEMON=$!
 
 sleep 10
 
-touch data
+mkdir -p data
 ./ipfs-collect &
 PID_COLLECT=$!
 
 cleanup() {
     echo "cleaning up"
     kill -9 $PID_COLLECT
-    kill -9 $PID_DAEMON
+    killall -9 ipfs-collect
+    #kill -9 $PID_DAEMON
 }
 
 trap "cleanup; exit" SIGHUP SIGINT SIGTERM
 
 echo "ipfs get"
-ipfs get -o data QmPVP4sDre9rtYahGvcjv3Fqet3oQyqrH5xS33d4YBVFme
-
+ipfs get -o data Qme6epvZDj3vzHcFKdF1nZhbixjw8Bn4imGcKnbUyBJL89
 
 cleanup
 sleep 5
